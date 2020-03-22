0123f863032bdf2f52c7dda2ce25a07e9b5c138c
christian <christian.froh@googlemail.com>
2020-03-22 03:42:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import fetch from 'unfetch';
+import { ApolloProvider } from 'react-apollo';
+import RegisterBed from './components/RegisterBed';
+
+const client = new ApolloClient({
+  uri: 'https://bed-tracking.gigalixirapp.com/api',
+  fetch
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <RegisterBed />
+    </ApolloProvider>
   );
 }
 
